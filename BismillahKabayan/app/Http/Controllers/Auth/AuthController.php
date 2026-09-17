626568d8c9fa4b7ca0d5abf70e6095d4f1e37764
reya-prm <reyapermana122@gmail.com>
2026-09-17 13:01:09 +0700

    mengubah nama halaman utama

--- BismillahKabayan/app/Http/Controllers/Auth/AuthController.php
+++ BismillahKabayan/app/Http/Controllers/Auth/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
 
     public function main()
     {
-        return view('index');
+        return view('home');
     }
 
     public function showLoginForm()
